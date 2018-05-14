b59fb942a4a2f559e38451c5dc1b3676e38bfa48
RichardZander <38966119+RichardZander@users.noreply.github.com>
2018-05-14 07:49:25 +0000

    Delete slblocks.m

--- slblocks.m
+++
@@ -1,6 +0,0 @@
-function blkStruct = slblocks
-Browser(3).Library = 'IP';% .slx Library file name
-Browser(3).Name    = 'Inverted Pendulum Libs';% Simulink  Library Browser name
-Browser(3).IsFlat  = 0;% default
-blkStruct.Browser = Browser;
-% End of slblocks
